29c10c7050f4385dcca20faf143d54b92986b2cf
rheajshah <rheajshah@gmail.com>
2024-04-09 01:55:11 -0500

    home buttons css

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <div className="title"> 
         IntraTracker
       </div>
-      <div className="btn-container rounded-pill mt-3 bubble-light">
+      <div className="btn-container rounded-pill mt-3 bubble-light disappear">
         <Link to="/team" className="btn btn-primary rounded-pill ms-3 button-style home-button">Manage Team</Link>
         <Link to="/timer" className="btn btn-primary rounded-pill ms-3 button-style home-button">Start New Game</Link>
         <Link to="/previous-games" className="btn btn-primary rounded-pill ms-3 button-style home-button">Previous Games</Link> {/* Add this line */}
